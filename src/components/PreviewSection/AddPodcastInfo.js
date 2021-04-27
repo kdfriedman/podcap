7e52d78987cdc815b4d3ccce3fa289ed08aea623
kfriedman <kfriedman@H10897.ad.hugeinc.com>
2021-04-26 22:51:29 -0400

    feat(addpodcastinfo): add form validation form error for submit without image and closeModal helper funciton

--- src/components/PreviewSection/AddPodcastInfo.js
+++ src/components/PreviewSection/AddPodcastInfo.js
@@ -1,4 +1,4 @@
-import { useRef, useContext } from "react";
+import { useRef, useContext, useState } from "react";
 import {
   Flex,
   Modal,
@@ -25,6 +25,8 @@ const AddPodcastInfo = ({ isOpen, onClose }) => {
   const [fileToBase64, updateBase64] = useContext(PreviewContext);
   const [formInputText, updateFormInputText] = useContext(PreviewContext);
 
+  const [formErrorMsg, updateFormErrorMsgState] = useState(null);
+
   // use to apply focus to  1st input on modal open
   const initialRef = useRef();
 
@@ -45,10 +47,42 @@ const AddPodcastInfo = ({ isOpen, onClose }) => {
         const url = await readInputFile(file);
         // update state with new image base64 string
         updateBase64(url);
+
+        // clear err msg state
+        updateFormErrorMsgState(null);
       } catch (err) {
         console.error(err);
       }
     })();
+  };
+
+  const handleAddPodcastInfoSubmit = (e) => {
+    e.preventDefault();
+    // check that current target is form, otherwise return
+    const form = e.currentTarget.closest("form");
+    if (!form) return;
+
+    // form inputs
+    const podcastNameInput = form.elements["podcastNameInput"];
+    const podcastTitleInput = form.elements["podcastTitleInput"];
+    const podcastFileUploadInput = form.elements["podcastFileUploadInput"];
+
+    // check if image is currently present from image upload input, return if not
+    if (!currentImage) {
+      return updateFormErrorMsgState(
+        "Please upload an image to save your brand info."
+      );
+    }
+
+    // close modal
+    onClose();
+  };
+
+  const closeModal = (e) => {
+    // clear err msg state
+    updateFormErrorMsgState(null);
+    // close modal
+    onClose();
   };
 
   return (
@@ -57,7 +91,7 @@ const AddPodcastInfo = ({ isOpen, onClose }) => {
         className="builder__section-modal"
         initialFocusRef={initialRef}
         isOpen={isOpen}
-        onClose={onClose}
+        onClose={closeModal}
       >
         <ModalOverlay className="builder__section-modal-overlay" />
         <ModalContent
@@ -65,160 +99,194 @@ const AddPodcastInfo = ({ isOpen, onClose }) => {
           maxW="530px"
           maxH="577px"
         >
-          <ModalHeader
-            className="builder__section-modal-header"
-            mt="25px"
-            fontSize="24px"
-            fontWeight="800"
-            fontFamily="Inter, san-serif"
-            display="flex"
-            justifyContent="center"
+          <form
+            onSubmit={handleAddPodcastInfoSubmit}
+            className="builder__section-modal-form"
           >
-            Your Podcast Info
-          </ModalHeader>
-          <ModalCloseButton className="builder__section-modal-close-btn" />
-          <ModalBody className="builder__section-modal-body" pb={6}>
-            <FormControl className="builder__section-modal-form-control">
-              <FormLabel
-                className="builder__section-modal-form-label"
-                fontWeight="700"
-                fontSize="15px"
-                color="#666666"
-                fontFamily="Inter, san-serif"
-              >
-                Podcast Name
-              </FormLabel>
-              <Input
-                className="builder__section-modal-form-input"
-                h="44px"
-                ref={initialRef}
-                placeholder="Enter podcast name"
-                _focus={{
-                  border: "1px solid #773AE7",
-                }}
-              />
-            </FormControl>
-
-            <FormControl className="builder__section-modal-form-control" mt={4}>
-              <FormLabel
-                className="builder__section-modal-form-label"
-                fontWeight="700"
-                fontSize="15px"
-                color="#666666"
-                fontFamily="Inter, san-serif"
-              >
-                Pocast Title
-              </FormLabel>
-              <Input
-                className="builder__section-modal-form-input"
-                _focus={{
-                  border: "1px solid #773AE7",
-                }}
-                h="44px"
-                borderRadius="4px"
-                placeholder="Enter podcast title"
-              />
-            </FormControl>
-
-            <FormControl className="builder__section-modal-form-control" mt={4}>
-              <FormLabel
-                className="builder__section-modal-form-label"
-                fontWeight="700"
-                fontSize="15px"
-                color="#666666"
-                fontFamily="Inter, san-serif"
-              >
-                Pocast Logo
-              </FormLabel>
-              <FormLabel
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                className="builder__section-modal-form-label"
-                w="150px"
-                h="150px"
-                borderRadius="4px"
-                border="1px solid #C6C6C6"
-              >
-                {!currentImage && (
-                  <Icon
-                    color="#aaaaaa"
-                    w="1.5rem"
-                    h="1.5rem"
-                    className="builder__section-modal-form-upload-icon"
-                    as={IoMdCloudUpload}
-                  />
-                )}
-                {!currentImage && (
-                  <Text
-                    color="#aaaaaa"
-                    fontSize="17px"
-                    fontFamily="Inter, san-serif"
-                    textAlign="center"
-                    fontWeight="400"
-                  >
-                    Upload Image
-                  </Text>
-                )}
-                {fileToBase64 && (
-                  <Flex
-                    h="10rem"
-                    className="builder__section-modal-image-container"
-                  >
-                    <Image
-                      maxWidth="100%"
-                      height="100%"
-                      overflow="hidden"
-                      objectFit="cover"
-                      alt="podcast brand"
-                      className="builder__section-modal-image-preview"
-                      src={fileToBase64}
-                      id="uploadedImage"
-                    />
-                  </Flex>
-                )}
+            <ModalHeader
+              className="builder__section-modal-header"
+              mt="25px"
+              fontSize="24px"
+              fontWeight="800"
+              fontFamily="Inter, san-serif"
+              display="flex"
+              justifyContent="center"
+            >
+              Your Podcast Info
+            </ModalHeader>
+            <ModalCloseButton
+              onClick={closeModal}
+              className="builder__section-modal-close-btn"
+            />
+            <ModalBody className="builder__section-modal-body" pb={6}>
+              <FormControl className="builder__section-modal-form-control">
+                <FormLabel
+                  className="builder__section-modal-form-label"
+                  fontWeight="700"
+                  fontSize="15px"
+                  color="#666666"
+                  fontFamily="Inter, san-serif"
+                >
+                  Podcast Name
+                </FormLabel>
                 <Input
+                  id="podcastNameInput"
                   className="builder__section-modal-form-input"
                   h="44px"
-                  borderRadius="4px"
-                  type="file"
-                  display="none"
-                  accept="image/png, image/jpeg, image/jpg"
-                  onChange={handleImageFileChange}
+                  ref={initialRef}
+                  placeholder="Enter podcast name"
+                  _focus={{
+                    border: "1px solid #773AE7",
+                  }}
+                  minLength="1"
+                  maxLength="100"
+                  required
                 />
-              </FormLabel>
-            </FormControl>
-          </ModalBody>
+              </FormControl>
 
-          <ModalFooter className="builder__section-modal-footer">
-            <Button
-              _hover={{
-                opacity: ".8",
-              }}
-              background="transparent"
-              onClick={onClose}
-              color="#4D4D4D"
-              fontFamily="Inter, san-serif"
-              fontWeight="700"
-              fontSize="16px"
-            >
-              CANCEL
-            </Button>
-            <Button
-              _hover={{
-                opacity: ".8",
-              }}
-              fontSize="16px"
-              fontWeight="700"
-              fontFamily="Inter, san-serif"
-              color="#fff"
-              background="#773AE7"
-              ml={3}
-            >
-              SAVE
-            </Button>
-          </ModalFooter>
+              <FormControl
+                className="builder__section-modal-form-control"
+                mt={4}
+              >
+                <FormLabel
+                  className="builder__section-modal-form-label"
+                  fontWeight="700"
+                  fontSize="15px"
+                  color="#666666"
+                  fontFamily="Inter, san-serif"
+                >
+                  Pocast Title
+                </FormLabel>
+                <Input
+                  id="podcastTitleInput"
+                  className="builder__section-modal-form-input"
+                  _focus={{
+                    border: "1px solid #773AE7",
+                  }}
+                  h="44px"
+                  borderRadius="4px"
+                  placeholder="Enter podcast title"
+                  minLength="1"
+                  maxLength="100"
+                  required
+                />
+              </FormControl>
+
+              <FormControl
+                className="builder__section-modal-form-control"
+                mt={4}
+              >
+                <FormLabel
+                  className="builder__section-modal-form-label"
+                  fontWeight="700"
+                  fontSize="15px"
+                  color="#666666"
+                  fontFamily="Inter, san-serif"
+                >
+                  Pocast Logo
+                </FormLabel>
+                <FormLabel
+                  cursor="pointer"
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  className="builder__section-modal-form-label"
+                  w="150px"
+                  h="150px"
+                  borderRadius="4px"
+                  border="1px solid #C6C6C6"
+                >
+                  {!currentImage && (
+                    <Icon
+                      color="#aaaaaa"
+                      w="1.5rem"
+                      h="1.5rem"
+                      className="builder__section-modal-form-upload-icon"
+                      as={IoMdCloudUpload}
+                    />
+                  )}
+                  {!currentImage && (
+                    <Text
+                      color="#aaaaaa"
+                      fontSize="17px"
+                      fontFamily="Inter, san-serif"
+                      textAlign="center"
+                      fontWeight="400"
+                    >
+                      Upload Image
+                    </Text>
+                  )}
+                  {fileToBase64 && (
+                    <Flex
+                      h="10rem"
+                      className="builder__section-modal-image-container"
+                    >
+                      <Image
+                        maxWidth="100%"
+                        height="100%"
+                        overflow="hidden"
+                        objectFit="cover"
+                        alt="podcast brand"
+                        className="builder__section-modal-image-preview"
+                        src={fileToBase64}
+                        id="uploadedImage"
+                      />
+                    </Flex>
+                  )}
+                  <Input
+                    className="builder__section-modal-form-input"
+                    h="44px"
+                    borderRadius="4px"
+                    type="file"
+                    display="none"
+                    accept="image/png, image/jpeg, image/jpg"
+                    onChange={handleImageFileChange}
+                  />
+                </FormLabel>
+                {formErrorMsg && (
+                  <Text
+                    fontWeight="400"
+                    fontFamily="Inter, san-serif"
+                    color="#E53E3E"
+                    fontSize="14px"
+                  >
+                    {formErrorMsg}
+                  </Text>
+                )}
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter className="builder__section-modal-footer">
+              <Button
+                _hover={{
+                  opacity: ".8",
+                }}
+                background="transparent"
+                onClick={closeModal}
+                color="#4D4D4D"
+                fontFamily="Inter, san-serif"
+                fontWeight="700"
+                fontSize="16px"
+              >
+                CANCEL
+              </Button>
+              <Button
+                type="submit"
+                _hover={{
+                  opacity: ".8",
+                }}
+                fontSize="16px"
+                fontWeight="700"
+                fontFamily="Inter, san-serif"
+                color="#fff"
+                background="#773AE7"
+                ml={3}
+              >
+                SAVE
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
